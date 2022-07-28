82995096d54b8abb8b513fcda14f00d6392252ec
Matthew Giovannini <shtyasek2003@mail.ru>
2022-07-28 22:44:55 +0500

    basket (init)

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -18,13 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('', include('mainapp.urls', namespace='main')),
-    path('auth/', include('authapp.urls', namespace='auth')),
-    path('basket/', include('basketapp.urls', namespace='basket')),
-    path('admin/', admin.site.urls),
-]
+app_name = 'basketapp'
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+
+]
